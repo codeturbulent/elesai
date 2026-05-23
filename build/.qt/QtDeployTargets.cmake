9f7328b412956e9f41ac3e37b4ca786e1776c010
Himanshu Yadav <codeturbulent@gmail.com>
2026-05-23 23:58:40 +0530

    wip

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_elsai_FILE /home/yetri/Everything-Code/C++-C/elsai/build/elsai)
-set(__QT_DEPLOY_TARGET_elsai_TYPE EXECUTABLE)
--- build/CMakeFiles/elsai.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for elsai.
-# This may be replaced when dependencies are built.
